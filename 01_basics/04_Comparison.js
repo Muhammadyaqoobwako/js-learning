c6681d5e65972b10935bc82a510cf0c17149bc4c
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2024-11-18 06:38:25 +0000

    Comparison

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("0.2" > 1);
+// we you are comparing any two numbers tha you have to asure the two numbers contain same dataType 
+
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null > 0);
+// in comparison null is treated as a number means zero
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+//=== => IT check equality as well as data type of the number i checks strictly
+
+console.log("2" === 2);
+
+
+
+
+
+
