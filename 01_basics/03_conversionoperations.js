0d1351ec212a2a04f5eb2344fbe4c73c9cdd3b4e
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2024-11-18 05:45:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,32 @@
+let score = "44aa"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// '33' => 33 easily convereted 
+// "33abc" => NAN not a number 
+// true => 1; false => 0
+ 
+
+let isLoggedIn = "sahil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "sahil" => true 
+
+let someNumber = 66;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
