5aa5535f0721d043ef3b198b56b9246808e1857c
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2024-12-01 07:15:32 +0000

    DataTypes_Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+// primitive data types 
+
+// 7 TYPES:string, Number, boolean, null, undefiend, symbol, bigint
+// symbol we use to make any variable unique 
+
+//reference (non primitive)
+// arrays, objects,functions
+
+
+//JavaScript is a dynamically typed language. 
+//This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it.
+// You can assign different types of values to a variable during its lifetime.
