7f9aedb9ec9ffeb43901af51de177944453b712c
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2025-01-27 18:27:46 +0000

    string depth

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,19 @@
+"hello" + "World"//To attach to string we use this 
+
+const Name = "Yaqoob";
+const Repo_Count = 20;
+
+//console.log(Name + Repo_Count + "Value"); outdated syntax
+
+// console.log(`hello my name is ${Name} and my repo count is ${Repo_Count}`);
+
+const gameName = new String('sahilq')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.   charAt(3));
+console.log(gameName.indexOf('a'));
+
+
