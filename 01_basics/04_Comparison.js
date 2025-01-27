8028839da7787c20078e12b5bdf3393b2bc9ce5f
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2025-01-27 15:08:32 +0000

    changes

--- 01_basics/04_Comparison.js
+++ 01_basics/04_Comparison.js
@@ -7,7 +7,7 @@
 
 // console.log("2" > 1);
 // console.log("0.2" > 1);
-// we you are comparing any two numbers tha you have to asure the two numbers contain same dataType 
+// we you are comparing any two numbers tha you have to asure the two numbers contain same dataType
 
 // console.log(null >= 0);
 // console.log(null == 0);
@@ -22,10 +22,7 @@
 
 //=== => IT check equality as well as data type of the number i checks strictly
 
-console.log("2" === 2);
+//console.log("2" === 2);
 
-
-
-
-
+///+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -10,3 +10,29 @@
 //JavaScript is a dynamically typed language. 
 //This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it.
 // You can assign different types of values to a variable during its lifetime.
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack(Primitive)=> in this we get copy of any value, 
+// Heap(Non-Primitive)=>in this we get the actual refrence of the value 
+
+//stack(permitive) 
+//Heap(non-permitive)
+
+let myYoutubename = "Sahil"
+
+let anothername = myYoutubename
+anothername = "yaqoob"
+console.log(myYoutubename);
+console.log(anothername);
+
+let user{
+    email = "sahil@gmail.com",
+    cnic = "112212"
+}
+
+let usertwo = user
+usertwo.email = "yaqoob@gmail.com"
+
+console.log(user);
+console.log(usertwo);
