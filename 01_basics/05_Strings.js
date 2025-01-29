9f91c6e08eef15d453394bde923f2dca5e93ee74
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2025-01-29 20:25:09 +0000

    with changes

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -1,19 +1,28 @@
-"hello" + "World"//To attach to string we use this 
+// "hello" + "World"//To attach to string we use this 
 
-const Name = "Yaqoob";
-const Repo_Count = 20;
+// const Name = "Yaqoob";
+// const Repo_Count = 20;
 
 //console.log(Name + Repo_Count + "Value"); outdated syntax
 
 // console.log(`hello my name is ${Name} and my repo count is ${Repo_Count}`);
 
-const gameName = new String('sahilq')
+// const gameName = "sahilq";  // Define as a string
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.   charAt(3));
-console.log(gameName.indexOf('a'));
+// console.log(gameName.charAt(3));  // Corrected syntax
+// console.log(gameName.indexOf('a'));
 
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
 
+// const anotherString = gameName.slice(0, 4); // Removed incorrect third argument
+// console.log(anotherString);
+
+const newStringone = "   Yaqoob    ";
+console.log(newStringone);
+console.log(newStringone.trim());
+
+const url = "http://yaqoob.com/yaqoob%20wako"
+console.log(url.replace('20%', '-'))
+
+console.log(url.includes('sahil'))
