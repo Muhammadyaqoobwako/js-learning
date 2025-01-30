49f1055c7b7b9892b1c32e9c872e796836d36e89
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2025-01-30 19:05:13 +0000

    numbers and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,46 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));//jb ap koi e-commerce website bna rahi hoti vha pe jab value had se zayada bari ho jati h toh tb hm esi use karti h 
+//console.log(balance.toFixed(1));//ham yaha p 1 bhi use kar sakti h magr zayada tar 2 he use hota h 
+
+const otherNumber = 1123.8966
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000;
+//console.log(hundreds.toLocaleString('en-PK'));
+
+//Number.MAX_VALUE
+///++++++++++++++++++++Maths++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//absolute converts negative into posiive but not positive to negative 
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,7,1,0,6,8));
+// console.log(Math.max(6.10,100));
+
+console.log(Math.random());// is value hamesha 0 and one ke bech main ayi g 
+console.log(Math.random()*10+1); 
+console.log((Math.random()*10) + 1); 
+
+const min = 10;
+const max = 20;
+
+console.log(Math.random() *( max- min + 1) + min);
+console.log(Math.floor(Math.random()) *( max- min + 1) + min);
+
+
+
+
+
+
+
+
+
