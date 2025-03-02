de43e8060538efa05a676346c74563e5a0e4cfc6
Muhammad Yaqoob Wako <164979487+Muhammadyaqoobwako@users.noreply.github.com>
2025-03-02 05:59:17 +0000

    jason talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -59,10 +59,45 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));//keys shows us data it array
-console.log(Object.values(tinderUser));//values shows the assign value
-console.log(Object.entries(tinderUser));//entries shows the assign values and the variable as the key in different partion
+//console.log(Object.keys(tinderUser));//keys shows us data it array
+//console.log(Object.values(tinderUser));//values shows the assign value
+//console.log(Object.entries(tinderUser));//entries shows the assign values and the variable as the key in different partion
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));//it checks that tinderuser the object i have created has property isLoggedIn or not it will return us true or false.
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));//it checks that tinderuser the object i have created has property isLoggedIn or not it will return us true or false.
+
+//--Destructring the objects
+const course = {
+    coursename: "js in Urdu",
+    price: "999",
+    courseInstructor: "Yaqoob"
+}
+
+//course.courseInstructor => if i need this value for three times than i don't write it for 3 times i can but i will go for any another method
+
+const {courseInstructor: instructor} = course // here i have changed the variable name courseInstructor to instructor it is called destructring the object
+
+console.log(instructor);
+//jason format Api => is main yeh hota h k agr hm apna kam kesi or k bhorosi chor d toh us ko. kesi or se kam lena 
+
+//this is the idea about api how they look like as a object
+// {
+//     "name":"Yaqoob",
+//     "coursename":"js",
+//     "price":"free"
+// }
+
+//jason format Apis in array in array there will be objects  //jason is a javascript object notation
+// [
+//     {
+
+//     }
+//     {
+
+//     }
+//     {
+
+//     }
+
+// ]
